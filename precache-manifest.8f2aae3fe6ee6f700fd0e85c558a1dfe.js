66cfcdb316a75bb78e79c8ffcf778c31fb859878
Rajat Dutta <rajat@Rajats-MacBook-Air.local>
2020-07-15 05:23:42 +0530

    Updates

---
+++ precache-manifest.8f2aae3fe6ee6f700fd0e85c558a1dfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd98f6a160415d209de5bad57106f283",
+    "url": "/Project_poc1/index.html"
+  },
+  {
+    "revision": "331b1f52cee5b845dff6",
+    "url": "/Project_poc1/static/css/main.a218f70a.chunk.css"
+  },
+  {
+    "revision": "cc7f22281008777190f8",
+    "url": "/Project_poc1/static/js/2.5d103e67.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Project_poc1/static/js/2.5d103e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "331b1f52cee5b845dff6",
+    "url": "/Project_poc1/static/js/main.aa8aeda2.chunk.js"
+  },
+  {
+    "revision": "8521f04eb7e3990f90e4",
+    "url": "/Project_poc1/static/js/runtime-main.f1b8f848.js"
+  }
+]);
